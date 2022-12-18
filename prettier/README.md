5a2194c4da429e19ec4ec948432f76883bb846c5
iyuuya <i.yuuya@gmail.com>
2022-12-18 10:47:11 +0900

    feat(prettier): add @iyuuya/styleguide/prettier package

---
+++ prettier/README.md
@@ -0,0 +1,11 @@
+# @iyuuya/styleguide/prettier
+
+## Configuration
+
+```javascript
+const { config } = require('@iyuuya/styleguide/prettier')
+
+module.exports = {
+  ...config
+};
+```
---
+++ prettier/index.js
@@ -0,0 +1,15 @@
+const config = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  jsxSingleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf"
+};
+
+module.exports = { config };
